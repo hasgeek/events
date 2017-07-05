42f0b71fd878d05c26df5ee5fbcd2dac06dd69ad
Karthik Balakrishnan <karthikb351@gmail.com>
2017-07-05 18:37:06 +0530

    Add fragments pages. Test deploy with travis

---
+++ deploy/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+openssl aes-256-cbc -K $encrypted_f874a539f83d_key -iv $encrypted_f874a539f83d_iv -in deploy/id_rsa.enc -out /tmp/deploy_rsa -d
+
+eval "$(ssh-agent -s)"
+
+chmod 600 /tmp/deploy_rsa
+
+ssh-add /tmp/deploy_rsa
+
+cd _site
+
+git init
+
+git remote add deploy git@e2e.hasgeek.com:fragments
+
+git push deploy
--- index.md
+++
@@ -1,4 +0,0 @@
----
-
----
-You probably want to head over to <a href="https://github.com/hasgeek/events">this GitHub repository</a>.
