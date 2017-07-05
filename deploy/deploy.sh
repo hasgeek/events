5da7043422951cbca10786fe165c8f233e3dd0ba
Karthik Balakrishnan <karthikb351@gmail.com>
2017-07-05 21:29:36 +0530

    Merge branch 'master' of https://github.com/hasgeek/events

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-openssl aes-256-cbc -K $encrypted_f874a539f83d_key -iv $encrypted_f874a539f83d_iv -in deploy/id_rsa.enc -out /tmp/deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_f874a539f83d_key -iv $encrypted_f874a539f83d_iv -in deploy/id_rsa.enc -out /tmp/deploy_rsa_$1 -d
 
 eval "$(ssh-agent -s)"
 
-chmod 600 /tmp/deploy_rsa
+chmod 600 /tmp/deploy_rsa_$1
 
-ssh-add /tmp/deploy_rsa
+ssh-add /tmp/deploy_rsa_$1
 
-cd _site
+bundle exec jekyll build --config configs/$1_config.yml
+cd _site/$1
 
 git init
 
@@ -19,6 +20,6 @@ git add .
 
 git commit -m "Deployment"
 
-git remote add deploy git@e2e.hasgeek.com:fragments
+git remote add deploy git@e2e.hasgeek.com:$1
 
 git push -u -f deploy master
