f2f945bdb12f352ff09a9bc41f5005eef050d4d2
Akshay Bhalotia <abhalotia99@gmail.com>
2017-11-30 17:25:57 +0530

    Add tests for 'discussions'

--- tests/before_build/steps/events.py
+++ tests/before_build/steps/events.py
@@ -133,6 +133,43 @@ proposed_speakers_schema = {
     },
 }
 
+discussions_schema = {
+    'funnel_url': {
+        'validator': valid_external_url,
+        'required': False
+    },
+    'talk_title': {
+        'type': 'string',
+        'required': True
+    },
+    'name': {
+        'type': 'string',
+        'required': True
+    },
+    'image_url': {
+        'validator': valid_external_url,
+        'required': False
+    },
+    'website': {
+        'type': 'dict',
+        'schema': {
+            'label': {
+                'type': 'string',
+                'required': True
+            },
+            'url': {
+                'validator': valid_external_url,
+                'required': True
+            }
+        },
+        'required': False
+    },
+    'blurb': {
+        'type': 'string',
+        'required': False
+    },
+}
+
 event_schema = {
     'layout': {
         'type': 'string',
