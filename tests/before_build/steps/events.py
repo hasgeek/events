47f9068f3389fdc8c8ad2e38feb81160e716a557
Abhishek Balaji <abhishek.balaji2@gmail.com>
2018-10-11 13:59:32 +0530

    Added past speakers validation (#1066)

--- tests/before_build/steps/events.py
+++ tests/before_build/steps/events.py
@@ -132,6 +132,42 @@ proposed_speakers_schema = {
         'required': False
     },
 }
+past_speakers_schema = {
+    'funnel_url': {
+        'validator': valid_external_url,
+        'required': False
+    },
+    'talk_title': {
+        'type': 'string',
+        'required': True
+    },
+    'name': {
+        'type': 'string',
+        'required': True
+    },
+    'image_url': {
+        'validator': valid_external_url,
+        'required': False
+    },
+    'website': {
+        'type': 'dict',
+        'schema': {
+            'label': {
+                'type': 'string',
+                'required': True
+            },
+            'url': {
+                'validator': valid_external_url,
+                'required': True
+            }
+        },
+        'required': False
+    },
+    'blurb': {
+        'type': 'string',
+        'required': False
+    },
+}
 
 flickr_album_schema = {
     'title': {
